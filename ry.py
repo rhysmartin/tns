fb86b46c1c39e3e6942618654e45c2423d0e3520
Rhys Martin <martin.rhys@gmail.com>
2016-03-26 01:51:09 -0500

    Added files via upload
    some name improvments, dicts switched to lists

--- ry.py
+++ ry.py
@@ -6,14 +6,18 @@ def rollDice(sides):
     return result
 
 class  Employee(object):
-    def __init__(self):
-        ##self.payRate
-
+    def __init__(self, name):
+        self.name = name
+        self.interviewScore = None
+        self.payRate = None
+        self.performanceScore = None
         genderRoll = rollDice(2)
         if genderRoll == 1:
             self.gender = 'male'
-        else:
+        elif genderRoll == 2:
             self.gender = 'female'
+        else:
+            self.gender = 'trans'
 
         raceRoll = rollDice(3)
         if raceRoll == 3:
@@ -21,25 +25,39 @@ class  Employee(object):
             self.race = 'URM'
         elif raceRoll == 2:
             #well-represented minority
-            self.race == 'WRM'
+            self.race = 'WRM'
         else:
             #over-represented majority
-            self.race == 'ORM'
+            self.race = 'ORM'
 
 
-class Company(object):
-    def __init__(self):
+class Employer(object):
+    def __init__(self, name):
+        self.fairnessScore = None
+        self.name = name
         self.industry = raw_input("With what industry is your company associated?")
-        self.name = raw_input("What is your company's name?")
-        self.employees = {}
-    def interview(employee):
-        Employee.interScire
-    def hire(self, Employee):
-        self.employees.append(Employee)
+        self.interviewees = []
+        self.employees = []
+    def interview(self, employee, score, decision=0):
+        employee.interviewScore = score
+        self.interviewees.append(employee)
+        if decision == 1:
+            self.employees.append(employee)
+
+    def batch_interview(self, employees):
+        for i in employees:
+            ##this is hard-coded for now!
+            self.interview(i, 100)
+
+    def setPay(self, employee, rate):
+        employee.payRate = rate
+
+    def setPerformanceScore(self, employee, score):
+        employee.payRate = score
 
 
 
-class Account(object):
+"""class Account(object):
     def __init__(self, initial_balance=0):
         ##self.number = uuid.uuid()
         self.Employee= input("Name on the account?")
@@ -47,5 +65,5 @@ class Account(object):
     def deposit(self, amount):
         self.balance += amount
     def withdraw(self, amount):
-        self.balance -= amount
+        self.balance -= amount"""
 
